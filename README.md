5322add3f1c497139d3588fc8727381f535e2557
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-23 10:47:03 +0100

    prepared new training

--- README.md
+++ README.md
@@ -22,13 +22,14 @@ The first version of the model was trained to predict 10 classes, which correspo
 
 After achieving satisfactory results with the 10-class model, a second model was trained to predict 88 classes, which correspond to specific Porsche car model variants and years. For example, this model can predict whether an image is a 911 from 2008 or a Cayenne from 1990. The accuracy of this model on the training set was 80%, and the accuracy on the validation set was 46%.
 
-| Model                 | Total params  | Trainable params  | Non-trainable params | Accuracy Train % | Accuracy Val % | Number of classes |
-|-----------------------|---------------|-------------------|----------------------|------------------|----------------|-------------------|
-| without augmentation* | 11,239,850    | 11,239,850        | 0                    | 98               | 78             | 10                |
-| with augmentation*    | 11,239,850    | 11,239,850        | 0                    | 79               | 74             | 10                |
-| old_pretrained*       | 20,027,082    | 5,311,114         | 14,715,968           | 74               | 72             | 10                |
-| VGG16 pretrained*     | 20,027,082    | 12,390,538        | 7,636,544            | 99               | 95             | 10                |
-| VGG16 pretrained      | 20,027,082    | 12,390,538        | 7,636,544            | 80               | 46             | 88                |
+| Model                 | Total params | Trainable params | Non-trainable params | Accuracy Train % | Accuracy Val % | Number of classes |
+|-----------------------|--------------|------------------|----------------------|------------------|----------------|-------------------|
+| without augmentation* | 11,239,850   | 11,239,850       | 0                    | 98               | 78             | 10                |
+| with augmentation*    | 11,239,850   | 11,239,850       | 0                    | 79               | 74             | 10                |
+| old_pretrained*       | 20,027,082   | 5,311,114        | 14,715,968           | 74               | 72             | 10                |
+| VGG16 pretrained*     | 20,027,082   | 12,390,538       | 7,636,544            | 99               | 95             | 10                |
+| VGG16 pretrained      | 20,027,082   | 12,390,538       | 7,636,544            | 80               | 46             | 88                |
+| Efficient(new head)   |              |                  |                      |                  |                |                   |
 
 The models with * were trained on the pre cleaned dataset.
 
--- training/pretrained_model.py
+++ training/pretrained_model.py
@@ -2,7 +2,7 @@
 # Import libraries
 import numpy as np
 from keras.models import Sequential
-from keras.applications import VGG16
+from keras.applications import VGG16, EfficientNetB0
 from tools import *
 from keras.optimizers import Adam
 from keras.regularizers import l1_l2
@@ -58,34 +58,60 @@ print(np.min(first_image), np.max(first_image))
 data_augmentation = create_augmentation_layer(img_height, img_width)
 show_augmented_batch(train_ds, data_augmentation)
 
-# Load the pre-trained VGG16 model
-vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=(img_height, img_width, 3))
-
+# Load the pre-trained VGG16/EfficientNet model
+# vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=(img_height, img_width, 3))
+efficientnet = EfficientNetB0(weights='imagenet', include_top=False, input_shape=(img_height, img_width, 3))
 # Set the trainable flag of the pre-trained model to False
-for layer in vgg16.layers:
+# for layer in vgg16.layers:
+# layer.trainable = False
+for layer in efficientnet.layers:
     layer.trainable = False
 
-# Fine-tuning: unfreeze some layers of VGG16
-for layer in vgg16.layers[-4:]:
+# Fine-tuning: unfreeze some layers of pretrained model
+# for layer in vgg16.layers[-4:]:
+# layer.trainable = True
+for layer in efficientnet.layers[-20:]:
     layer.trainable = True
 
 # Create a new head and initialize model
 num_classes = len(class_names)
+# model = Sequential([
+#    data_augmentation,
+#    #vgg16,
+#    layers.BatchNormalization(),
+#    layers.LeakyReLU(),
+#    layers.Flatten(),
+#    layers.Dense(128, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4)),
+#    layers.Dropout(0.5),
+#    layers.BatchNormalization(),
+#    layers.LeakyReLU(),
+#    layers.Dense(num_classes, activation='softmax', name="outputs")
+# ]) if not load_model else keras.models.load_model(load_path)
+# Create a new head and initialize model
+num_classes = len(class_names)
 model = Sequential([
     data_augmentation,
-    vgg16,
-    layers.BatchNormalization(),
-    layers.LeakyReLU(),
-    layers.Flatten(),
+    efficientnet,
+    layers.GlobalAveragePooling2D(),
     layers.Dense(128, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4)),
-    layers.Dropout(0.5),
     layers.BatchNormalization(),
     layers.LeakyReLU(),
+    layers.Dropout(0.5),
     layers.Dense(num_classes, activation='softmax', name="outputs")
 ]) if not load_model else keras.models.load_model(load_path)
 
 # Define optimizer
-optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+#optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+initial_learning_rate = 0.001
+lr_decay_steps = 1000
+lr_decay_rate = 0.96
+
+lr_scheduler = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate,
+    decay_steps=lr_decay_steps,
+    decay_rate=lr_decay_rate,
+    staircase=True)
+optimizer = Adam(learning_rate=lr_scheduler, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 # Compile model
 model.compile(optimizer=optimizer,
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -93,7 +119,8 @@ model.compile(optimizer=optimizer,
 model.summary()
 
 # Define callbacks
-lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, mode='auto', cooldown=0, min_lr=0)
+#lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, mode='auto', cooldown=0, min_lr=0)
+
 early_stopping = EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto', restore_best_weights=True)
 model_checkpoint = ModelCheckpoint(filepath="../models/model_variants/best_model.h5", monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
 # Train model
@@ -111,3 +138,27 @@ plot_model_score(history, epochs, name, specific_model_variants)
 # Save model
 model.save(f"../models/model_variants/{name}.h5")
 
+# TODO: Try efficientNEtB0 instead of VGG16, different optimizer and head
+# TODO: Try effiecientNEt on old head
+# TODO: Implement a DataGenerator
+# TODO: Different data augmentation (vertical, ..), Augmentation before training
+
+#from tensorflow.keras.preprocessing.image import ImageDataGenerator
+#
+#train_datagen = ImageDataGenerator(
+#    rescale=1./255,
+#    rotation_range=20,
+#    width_shift_range=0.2,
+#    height_shift_range=0.2,
+#    shear_range=0.2,
+#    zoom_range=0.2,
+#    horizontal_flip=True,
+#    fill_mode='nearest')
+#
+#validation_datagen = ImageDataGenerator(rescale=1./255)
+#
+## Flow the data
+#train_generator = train_datagen.flow_from_directory(train_dir,
+#                                                    target_size=(img_height, img_width),
+#                                                    batch_size=batch_size,
+#                                                    class_mode='c
