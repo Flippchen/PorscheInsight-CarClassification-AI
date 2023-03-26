fc1927d6976d2593f2ef9064f9d28a5c397b21ae
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-26 18:23:45 +0200

    new test with efficientnet and old head
    added discord webhook as env variable

--- training/pretrained_model.py
+++ training/pretrained_model.py
@@ -7,6 +7,7 @@ from utilities.discord_callback import DiscordCallback
 from keras.optimizers import Adam
 from keras.regularizers import l1_l2
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+import os
 
 # Ignore warnings
 import warnings
@@ -18,7 +19,7 @@ suppress_tf_warnings()
 AUTOTUNE = tf.data.AUTOTUNE
 img_height = 300
 img_width = 300
-name = "vgg16-pretrained-deeper-model-variants"
+name = "efficientnet-old-head-model-variants"
 # Variables to control training flow
 # Set specific_model_variants to True if you want to test the model with specific Porsche model variants and years.
 # Set specific_model_variants to False if you want to test the model with broad Porsche model types.
@@ -75,30 +76,32 @@ for layer in efficientnet.layers[-20:]:
 
 # Create a new head and initialize model
 num_classes = len(class_names)
-# model = Sequential([
-#    data_augmentation,
-#    #vgg16,
-#    layers.BatchNormalization(),
-#    layers.LeakyReLU(),
-#    layers.Flatten(),
-#    layers.Dense(128, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4)),
-#    layers.Dropout(0.5),
-#    layers.BatchNormalization(),
-#    layers.LeakyReLU(),
-#    layers.Dense(num_classes, activation='softmax', name="outputs")
-# ]) if not load_model else keras.models.load_model(load_path)
-# Create a new head and initialize model
-num_classes = len(class_names)
 model = Sequential([
     data_augmentation,
+    # vgg16,
     efficientnet,
-    layers.GlobalAveragePooling2D(),
-    layers.Dense(128, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4)),
     layers.BatchNormalization(),
     layers.LeakyReLU(),
+    layers.Flatten(),
+    layers.Dense(128, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4)),
     layers.Dropout(0.5),
+    layers.BatchNormalization(),
+    layers.LeakyReLU(),
     layers.Dense(num_classes, activation='softmax', name="outputs")
 ]) if not load_model else keras.models.load_model(load_path)
+
+# Create a new head and initialize model
+num_classes = len(class_names)
+# model = Sequential([
+#    data_augmentation,
+#    efficientnet,
+#    layers.GlobalAveragePooling2D(),
+#    layers.Dense(128, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4)),
+#    layers.BatchNormalization(),
+#    layers.LeakyReLU(),
+#    layers.Dropout(0.5),
+#    layers.Dense(num_classes, activation='softmax', name="outputs")
+# ]) if not load_model else keras.models.load_model(load_path)
 
 # Define optimizer
 optimizer = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
@@ -112,7 +115,6 @@ lr_scheduler = tf.keras.optimizers.schedules.ExponentialDecay(
     decay_rate=lr_decay_rate,
     staircase=True)
 
-
 # Compile model
 model.compile(optimizer=optimizer,
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
@@ -120,11 +122,13 @@ model.compile(optimizer=optimizer,
 model.summary()
 
 # Define callbacks
-#lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, mode='auto', cooldown=0, min_lr=0)
+# lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, verbose=1, mode='auto', cooldown=0, min_lr=0)
 lr = tf.keras.callbacks.LearningRateScheduler(lr_scheduler)
-early_stopping = EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto', restore_best_weights=True)
-model_checkpoint = ModelCheckpoint(filepath="../models/model_variants/best_model.h5", monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
-webhook_url = "YOUR WEBHOOK URL"
+early_stopping = EarlyStopping(monitor='val_loss', patience=4, verbose=1, mode='auto', restore_best_weights=True)
+model_checkpoint = ModelCheckpoint(filepath=f"../models/model_variants/{name}_best_model.h5", monitor='val_loss', verbose=1, save_best_only=True, mode='auto')
+# Discord callback ( If you want to use this, you need to set the environment variable "WEBHOOK_URL",
+# otherwise comment it out and also remove the callback from the model callbacks)
+webhook_url = os.environ.get('WEBHOOK_URL')
 discord_callback = DiscordCallback(webhook_url)
 # Train model
 epochs = 15
@@ -133,22 +137,21 @@ with tf.device('/GPU:1'):
         train_ds,
         validation_data=val_ds,
         epochs=epochs,
-        callbacks=[lr,early_stopping, model_checkpoint, discord_callback]
+        callbacks=[lr, early_stopping, model_checkpoint, discord_callback]
     )
 # Plot and save model score
-plot_model_score(history, epochs, name, specific_model_variants)
+plot_model_score(history, name, specific_model_variants)
 
 # Save model
 model.save(f"../models/model_variants/{name}.h5")
 
-# TODO: Try efficientNEt instead of VGG16, different head
 # TODO: Try effiecientNEt on old head
 # TODO: Implement a DataGenerator
 # TODO: Different data augmentation (vertical, ..), Augmentation before training
 
-#from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from tensorflow.keras.preprocessing.image import ImageDataGenerator
 #
-#train_datagen = ImageDataGenerator(
+# train_datagen = ImageDataGenerator(
 #    rescale=1./255,
 #    rotation_range=20,
 #    width_shift_range=0.2,
@@ -158,10 +161,10 @@ model.save(f"../models/model_variants/{name}.h5")
 #    horizontal_flip=True,
 #    fill_mode='nearest')
 #
-#validation_datagen = ImageDataGenerator(rescale=1./255)
+# validation_datagen = ImageDataGenerator(rescale=1./255)
 #
 ## Flow the data
-#train_generator = train_datagen.flow_from_directory(train_dir,
+# train_generator = train_datagen.flow_from_directory(train_dir,
 #                                                    target_size=(img_height, img_width),
 #                                                    batch_size=batch_size,
 #                                                    class_mode='c
