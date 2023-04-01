d175d59bd6e5d09508b5a5d94c3fa3af73f6e529
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-04-01 17:51:25 +0200

    fixed false import

--- web_app/main.py
+++ web_app/main.py
@@ -6,11 +6,10 @@ import eel
 import base64
 from io import BytesIO
 from PIL import Image
-from utilities.tools import suppress_tf_warnings, get_classes_for_model
+from utilities.tools import get_classes_for_model
 from testing.prepare_images import replace_background
 import pooch
 
-suppress_tf_warnings()
 
 # Initiate models
 models = {
