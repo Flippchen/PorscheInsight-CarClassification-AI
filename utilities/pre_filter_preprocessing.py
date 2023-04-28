ba167915d076d19a4bfc5aa392e342c76f32826f
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-04-28 21:45:24 +0200

    added replacement of background

--- utilities/pre_filter_preprocessing.py
+++ utilities/pre_filter_preprocessing.py
@@ -4,6 +4,7 @@ from rembg import remove, new_session
 import random
 from joblib import Parallel, delayed, parallel_backend
 
+
 def random_rgba_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
@@ -13,17 +14,34 @@ def random_rgba_color():
 
 
 def process_image(image_path):
-    color = random_rgba_color()
+    bg_image = random.choice(bg_images)
+
+    local_image = bg_image.copy()
     image = Image.open(image_path)
-    image = remove(image, bgcolor=color)
-    image = image.convert("RGB")
+    image = remove(image)
+
+    local_image.paste(image, mask=image)
+    local_image = local_image.convert("RGB")
     print(f"Saving image to {image_path}")
-    image.save(image_path)
+    local_image.save(image_path)
+
+
+def get_background_images(path):
+    images = []
+    for root, _, files in os.walk(path):
+        for file in files:
+            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.jfif')):
+                image_path = os.path.join(root, file)
+                image = Image.open(image_path)
+                image = image.convert("RGB")
+                image = image.resize((300, 300))
+                images.append(image)
+
+    return images
 
 
 def process_images_in_folder(main_folder, n_jobs=4):
     image_paths = []
-
     for root, _, files in os.walk(main_folder):
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
@@ -34,7 +52,9 @@ def process_images_in_folder(main_folder, n_jobs=4):
         Parallel()(delayed(process_image)(path) for path in image_paths)
 
 
+main_folder_path = r"C:\Users\phili\.keras\datasets\resized_DVM\pre_filter\porsche"
+bg_folder_path = r"C:\Users\phili\.keras\datasets\bg"
 
+bg_images = get_background_images(bg_folder_path)
 
-main_folder_path = r"C:\Users\phili\.keras\datasets\pre_filter_test\other_car_brand"
 process_images_in_folder(main_folder_path)
