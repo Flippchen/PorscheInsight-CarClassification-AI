c47b4e2b2f8f192f0c4584bb2c8ab833a0fac9c1
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-04-28 21:46:59 +0200

    added training results and deleted unnecessary training code

--- training/train.py
+++ training/train.py
@@ -53,8 +53,6 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 normalization_layer = layers.Rescaling(1. / 255)
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-print(np.min(first_image), np.max(first_image))
 
 # Create data augmentation layer and show augmented batch
 data_augmentation = create_augmentation_layer(img_height, img_width)
