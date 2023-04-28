48d8157700d5e7b2f715016a84c380f3fb77cc69
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-04-28 10:31:41 +0200

    added training results

--- training/pre_filter.py
+++ training/pre_filter.py
@@ -19,7 +19,7 @@ suppress_tf_warnings()
 AUTOTUNE = tf.data.AUTOTUNE
 img_height = 300
 img_width = 300
-name = "efficientnet-pre-filter"
+name = "efficientnet-pre-filter-refactored-dataset"
 # Variables to control training flow
 # Set model Type to 'all_specific_model_variants' or 'car_type or 'specific_model_variants'
 model_type = 'pre_filter'
