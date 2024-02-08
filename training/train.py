df05e45efafb6e8bc0280a2080f85d39e3f9ade3
Flippchen <91947480+Flippchen@users.noreply.github.com>
2024-02-08 12:01:23 +0100

    Chore: Deleted not used codeblock

--- training/train.py
+++ training/train.py
@@ -53,11 +53,6 @@ show_batch_shape(train_ds)
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-# Normalize the data
-normalization_layer = layers.Rescaling(1. / 255)
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-
 # Create data augmentation layer and show augmented batch
 data_augmentation = create_augmentation_layer(img_height, img_width)
 show_augmented_batch(train_ds, data_augmentation)
